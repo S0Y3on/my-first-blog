5324984be403ca268011f468df55a255a83f5f25
SoY3on <sylee1248@gmail.com>
2020-03-06 23:41:54 +0900

    Added login

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls import url
+
 from . import views
 
 
@@ -7,5 +9,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 ]
 #post_list view를 루트 url로 할당
