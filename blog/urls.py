0389fd526f06b954de8ca628ae43d0d94538bbbe
SoY3on <sylee1248@gmail.com>
2020-03-06 20:55:33 +0900

    Added view template for detailes blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
-#post_list view를 루트 url로 할당 
+#post_list view를 루트 url로 할당
